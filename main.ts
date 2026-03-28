e33f3a37259c8ae78795f5d4eb760b01ca4e69b1
Diffid10 <159997191+Diffid10@users.noreply.github.com>
2026-03-28 19:25:34 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
+//% color=#FF6F00 icon="\uf085"
 namespace SmoothServo {
 
-    //% block="servo pin %pin|from %start to %end in %time ms (smooth)"
-    export function moveSmooth(start: number): void {
-    basic.showNumber(0)
+    //% blockId=smooth_servo_move
+    //% block="servo %pin|from %start to %end in %time ms (smooth)"
+    //% weight=100
+    export function moveSmooth(pin: AnalogPin, start: number, end: number, time: number): void {
 
+        let steps = Math.abs(end - start)
+        if (steps == 0) return
+
+        for (let i = 0; i <= steps; i++) {
+
+            let t = i / steps
+            let ease = t * t * (3 - 2 * t) // smoothstep
+
+            let angle = start + (end - start) * ease
+
+            pins.servoWritePin(pin, angle)
+            basic.pause(time / steps)
         }
     }
+}
